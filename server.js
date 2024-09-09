eb4aaec4215dc77ae111e9d2c6a483d697dc2b71
PRATAP-KUMAR <pratap@fastmail.fm>
2024-09-10 00:48:32 +0530

    update cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://pratap-panabaka-express-api.netlify.app/'],
+    origin: ['http://localhost:5173', 'https://pratap-panabaka-aconews.web.app/'],
 }));
 
 // news
