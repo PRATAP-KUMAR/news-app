d365fd5ce92f3a79608c205a78c824dad5c1d7e1
PRATAP-KUMAR <pratap@fastmail.fm>
2024-09-10 07:38:31 +0530

    trying netlify deploy

---
+++ netlify/functions/controllers/headlines.js
@@ -0,0 +1,22 @@
+import mockData from "../mockData.js";
+import fetchHeadlines from "../utils/fetchHeadlines.js";
+
+const getHeadlinesFiltered = async (req, res) => {
+    try {
+        const json = await fetchHeadlines(req.body);
+        res.status(200).json(json);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const getHeadlines = async (req, res) => {
+    try {
+        const json = await fetchHeadlines();
+        res.status(200).json(json);
+    } catch (error) {
+        res.status(200).json(mockData);
+    }
+}
+
+export { getHeadlines, getHeadlinesFiltered }
