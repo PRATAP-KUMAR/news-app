361965f0df992d8e9279d8dc5ce24842fc71a698
PRATAP PANABAKA <pratap@fastmail.fm>
2024-09-26 07:46:01 +0530

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -12,7 +12,7 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://pratap-panabaka-aconews.web.app'],
+    origin: ['http://localhost:5173', 'https://pratap-panabaka-news.web.app'],
 }));
 
 app.use('/.netlify/functions/api/news', news);
